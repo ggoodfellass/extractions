9741195c12485a5192d732c78d856d77e3f61d55
Junaid Farooq <junaid@evercam.io>
2020-05-17 14:58:45 +0500

    Work in progress.

--- lib/extractions.ex
+++ lib/extractions.ex
@@ -1,18 +1,43 @@
 defmodule Extractions do
-  @moduledoc """
-  Documentation for `Extractions`.
-  """
 
-  @doc """
-  Hello world.
+  def start do
+    start_date = Calendar.DateTime.from_erl!({{2019, 10, 2},{0, 29, 10}}, "Etc/UTC", {123456, 6}) #|> Calendar.DateTime.shift_zone!("Europe/Dublin")
+    end_date = Calendar.DateTime.from_erl!({{2019, 12, 2},{0, 29, 10}}, "Etc/UTC", {123456, 6}) #|> Calendar.DateTime.shift_zone!("Europe/Dublin")
 
-  ## Examples
+    interval = 1200
 
-      iex> Extractions.hello()
-      :world
+    all_days = Calendar.Date.days_after_until(start_date, end_date, true)
+    schedule = %{
+      "Friday" => ["08:00-18:00"],
+      "Monday" => ["08:00-18:00"],
+      "Saturday" => [],
+      "Sunday" => [],
+      "Thursday" => ["08:00-18:00"],
+      "Tuesday" => ["08:00-18:00"],
+      "Wednesday" => ["08:00-18:00"]
+    }
+    |> Enum.filter(fn {_, v} -> length(v) != 0 end)
+    |> Enum.into(%{})
 
-  """
-  def hello do
-    :world
+    days =
+      schedule
+      |> Enum.map(fn(sc) ->
+        {day, hours} = sc
+        if length(hours) != 0, do: day
+      end) |> Enum.filter(& !is_nil(&1))
+
+    valid_dates =
+      Enum.filter(all_days, fn(day) ->
+        Enum.member?(days, day |> Calendar.Strftime.strftime!("%A"))
+      end)
+
+
+  end
+
+  def ambiguous_handle(value) do
+    case value do
+      {:ok, datetime} -> datetime
+      {:ambiguous, datetime} -> datetime.possible_date_times |> hd
+    end
   end
 end
