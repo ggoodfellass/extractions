e8fdf999eeecae8dff13e33efd63284ebbce4051
Junaid Farooq <junaid@evercam.io>
2020-06-12 06:47:25 +0500

    Added uploader.

--- lib/commons.ex
+++ lib/commons.ex
@@ -1,4 +1,5 @@
 defmodule Commons do
+  require Logger
 
   def get_head_tail([]), do: []
   def get_head_tail(nil), do: []
@@ -11,5 +12,42 @@ defmodule Commons do
 
   def save_current_jpeg_time(url, path, :v2) do
     File.write!("#{path}CURRENT", url <> "\n", [:append])
+  end
+
+  def seaweedfs_save(camera_exid, timestamp, image) do
+    [{_, _, _, _, [server]}] = :ets.match_object(:storage_servers, {:_, "RW", :_, :_, :_})
+    hackney = [pool: :seaweedfs_upload_pool]
+    directory_path = construct_directory_path(camera_exid, timestamp, "recordings", "")
+    file_name = construct_file_name(timestamp)
+    file_path = directory_path <> file_name
+    case HTTPoison.post("#{server.url}#{file_path}", {:multipart, [{file_path, image, []}]}, [], hackney: hackney) do
+      {:ok, response} -> response
+      {:error, error} -> Logger.info "[seaweedfs_save] [#{file_path}] [#{camera_exid}] [#{inspect error}]"
+    end
+  end
+
+  defp construct_directory_path(camera_exid, timestamp, app_dir, root_dir \\ @root_dir) do
+    timestamp
+    |> Calendar.DateTime.Parse.unix!
+    |> Calendar.Strftime.strftime!("#{root_dir}/#{camera_exid}/snapshots/#{app_dir}/%Y/%m/%d/%H/")
+  end
+
+  defp construct_file_name(timestamp) do
+    timestamp
+    |> Calendar.DateTime.Parse.unix!
+    |> Calendar.Strftime.strftime!("%M_%S_%f")
+    |> format_file_name
+  end
+
+  defp format_file_name(<<file_name::bytes-size(6)>>) do
+    "#{file_name}000" <> ".jpg"
+  end
+
+  defp format_file_name(<<file_name::bytes-size(7)>>) do
+    "#{file_name}00" <> ".jpg"
+  end
+
+  defp format_file_name(<<file_name::bytes-size(9), _rest :: binary>>) do
+    "#{file_name}" <> ".jpg"
   end
 end
---
+++ lib/extractions/uploader.ex
@@ -0,0 +1,43 @@
+defmodule Uploader do
+  @root_dir "/storage"
+
+  require Logger
+  import Commons
+
+  def start(from, to) do
+
+    uploading = fn (tuple) ->
+      upload(tuple)
+    end
+
+    dir = @root_dir <> "/" <> from <> "/"
+    stream_it(dir, to)
+    |> Task.async_stream(&(uploading.(&1)), max_concurrency: System.schedulers_online() * 2, timeout: :infinity)
+    |> Stream.run
+  end
+
+  defp upload(%{
+    timestamp: timestamp,
+    image: image,
+    camera_exid: camera_exid})
+  do
+    seaweedfs_save(camera_exid, timestamp, image)
+  end
+  defp upload([]), do: :noop
+
+  defp stream_it(dir, to) do
+    File.ls!(dir)
+    |> Enum.map(&compose(&1, dir, to))
+  end
+
+  defp compose("CURRENT", _dir, _to), do: []
+  defp compose(file, dir, to) do
+    timestamp = String.split(file, ".") |> List.first
+    image = File.read!(dir <> file)
+    %{
+      timestamp: timestamp,
+      image: image,
+      camera_exid: to
+    }
+  end
+end
